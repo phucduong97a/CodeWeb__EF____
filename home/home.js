fba333420f7bdc970417b5f9c8fec896db622ffc
PhucDV <phucdv@ominext.com>
2022-04-01 16:13:59 +0700

    code update

---
+++ home/home.js
@@ -0,0 +1,11 @@
+<script>
+{/*    let user = {*/}
+{/*    name: "John",*/}
+{/*    age: 30,*/}
+{/*    isAdmin: true*/}
+{/*};*/}
+{/*    for (let key in user) {*/}
+{/*    console.log(`Key ${key} có giá trị là ${user[key]}`);*/}
+// }
+    console.log("fsdfgds|")
+</script>
